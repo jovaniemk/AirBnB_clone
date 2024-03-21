51a869d2a04a519a53be700298bd2d391465b5b3
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2024-03-21 12:47:12 +0300

    writing initial draft of base_model

---
+++ models/base_model.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""this module will contain base_model class"""
+
+from uuid import uuid4
+from datetime import datetime
+class BaseModel:
+   """BaseModel class"""
+   def __init__(self):
+       self.id = str(uuid4())
+       tmp = datetime.now(tz=None)
+       self.created_at = tmp
+       self.updated_at = tmp
+   def __str__(self):
+      """string representation of an object"""
+      return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
+   def save(self):
+      """updates updated_at attribute"""
+      self.updated_at = datetime.now(tz=None)
+   def to_dict(self):
+      tmp = self.__dict__.copy()
+      tmp['__class__'] = self.__class__.__name__
+      tmp['created_at'] = self.created_at.isoformat()
+      tmp['updated_at'] = self.updated_at.isoformat()
+      return tmp
