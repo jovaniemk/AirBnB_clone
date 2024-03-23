a827f90e6da5f5afeb3780c9114e2bdd744e0968
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2024-03-23 13:39:35 +0300

    pycodestyle fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-#from models.user import User
+
+
 import json
 import os
+
+
 class FileStorage:
     """file storage class"""
     __file_path = "file.json"
@@ -16,13 +19,15 @@ class FileStorage:
 
     def new(self, obj):
         FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+
     def save(self):
-            """write json representation of an object to a file"""
-            tmp_dict = {}
-            for key, value in FileStorage.__objects.items():
-                tmp_dict[key] = value.to_dict()
-            with open(FileStorage.__file_path, 'w', encoding="utf-8") as f:
-                json.dump(tmp_dict, f)
+        """write json representation of an object to a file"""
+        tmp_dict = {}
+        for key, value in FileStorage.__objects.items():
+            tmp_dict[key] = value.to_dict()
+        with open(FileStorage.__file_path, 'w', encoding="utf-8") as f:
+            json.dump(tmp_dict, f)
+
     def reload(self):
         """returns the dictionary representation of an object from the file"""
         from models.base_model import BaseModel
@@ -33,10 +38,12 @@ class FileStorage:
         from models.place import Place
         from models.review import Review
         tmp = {}
-        l1 = ['BaseModel', 'User', 'State', 'City', 'Amenity', 'place', 'Review']
+        l1 = ['BaseModel', 'User', 'State', 'City', 'Amenity', 
+              'place', 'Review']
         l2 = [BaseModel, User, State, City, Amenity, Place, Review]
 
-        if os.path.isfile(FileStorage.__file_path) and os.path.getsize(FileStorage.__file_path) > 0:
+        if os.path.isfile(FileStorage.__file_path) and 
+        os.path.getsize(FileStorage.__file_path) > 0:
             with open(FileStorage.__file_path, 'r') as f:
                 tmp = json.loads(f.read())
         for key, value in tmp.items():
