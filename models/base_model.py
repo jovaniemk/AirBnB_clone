46c908e9672f38d6f1fc46794f2c01a45a375065
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2024-03-23 13:25:20 +0300

    pycodestyle fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,34 +1,41 @@
 #!/usr/bin/python3
 """this module will contain base_model class"""
 
+
 from models import storage
 from uuid import uuid4
 from datetime import datetime
+
+
 class BaseModel:
-   """BaseModel class"""
-   def __init__(self, *args, **kwargs):
-      if len(kwargs) > 0:
-         for key, value in kwargs.items():
-            if key != '__class__':
-               if key == 'created_at' or key == 'updated_at':
-                  value = datetime.fromisoformat(value)
-               setattr(self, key, value)
-      else:
-         self.id = str(uuid4())
-         tmp = datetime.now(tz=None)
-         self.created_at = tmp
-         self.updated_at = tmp
-         storage.new(self)
-   def __str__(self):
-      """string representation of an object"""
-      return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
-   def save(self):
-      """updates updated_at attribute"""
-      self.updated_at = datetime.now(tz=None)
-      storage.save()
-   def to_dict(self):
-      tmp = self.__dict__.copy()
-      tmp['__class__'] = self.__class__.__name__
-      tmp['created_at'] = self.created_at.isoformat()
-      tmp['updated_at'] = self.updated_at.isoformat()
-      return tmp
+    """BaseModel class"""
+
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.fromisoformat(value)
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            tmp = datetime.now(tz=None)
+            self.created_at = tmp
+            self.updated_at = tmp
+            storage.new(self)
+
+    def __str__(self):
+        """string representation of an object"""
+        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
+
+    def save(self):
+        """updates updated_at attribute"""
+        self.updated_at = datetime.now(tz=None)
+        storage.save()
+
+    def to_dict(self):
+        tmp = self.__dict__.copy()
+        tmp['__class__'] = self.__class__.__name__
+        tmp['created_at'] = self.created_at.isoformat()
+        tmp['updated_at'] = self.updated_at.isoformat()
+        return tmp
