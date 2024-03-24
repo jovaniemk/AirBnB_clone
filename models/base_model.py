06a28846510c6b0b4ac4410508907c12344ebdab
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2024-03-24 13:00:42 +0300

    adding documentation to the object

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""this module will contain BaseModel class"""
+"""this module will contain BaseModel class
+BaseModel class will be a blue print for other
+classes which will be defined using attributes of
+BaseModel"""
 
 
 from models import storage
@@ -10,9 +13,11 @@ from datetime import datetime
 class BaseModel:
     """BaseModel class, this class will define common attributes
     for all the classes which will be later defined
-    Args:
-    our class will eiter have no argument or dictionary representation of 
-    a previously created object"""
+    Attributes:
+    id(str): unique id for each object
+    created_at(datetime.datetime): time in wich the object is created
+    updated_at(datetime.datetime): time in which the object is updated
+    """
 
     def __init__(self, *args, **kwargs):
         """this method will intialize our BaseModel object
@@ -34,16 +39,23 @@ class BaseModel:
             storage.new(self)
 
     def __str__(self):
-        """this instance method will return string representation of an object"""
+        """this instance method will return string representation of an object
+        this string representation will contain class_name, 
+        id of the object and dict attribute of the object"""
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
     def save(self):
-        """updates updated_at attribute"""
+        """this method will save the new attributes of the object to 
+        the File_Storage class and will change to the updated_at attribute 
+        to the new time"""
         self.updated_at = datetime.now(tz=None)
         storage.save()
 
     def to_dict(self):
-        """this instance method will return a dictionary representation of an object"""
+        """this instance method will return a dictionary representation of an object
+        this method will copy the dict attribute of the object with some modifications
+        it will change cretaed_at and updated_at attribute to iso_format and add __class__
+        key to the dictionary"""
         tmp = self.__dict__.copy()
         tmp['__class__'] = self.__class__.__name__
         tmp['created_at'] = self.created_at.isoformat()
