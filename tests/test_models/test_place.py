b5227bdd3983ba0db996d45306097f51e8852c31
Kidanetes <117867959+Kidanetes@users.noreply.github.com>
2024-03-25 14:55:04 +0300

    adding test file for Place class

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""this module conatins unittest for Amenity class"""
+
+
+import unittest
+from models.place import Place
+from uuid import uuid4
+import datetime
+
+
+class test_place(unittest.TestCase):
+    """this class will contain methods which will test
+    types and values of attributes of place"""
+
+    def test_type(self):
+        """this method will test wheter types of
+        attributes are correct"""
+        place1 = Place()
+        self.assertEqual(type(place1.city_id), str)
+        self.assertEqual(type(place1.user_id), str)
+        self.assertEqual(type(place1.name), str)
+        self.assertEqual(type(place1.description), str)
+        self.assertEqual(type(place1.number_rooms), int)
+        self.assertEqual(type(place1.number_bathrooms), int)
+        self.assertEqual(type(place1.max_guest), int)
+        self.asserEqual(type(place1.price_by_night), int)
+        self.assertEqual(type(place1.latitude), float)
+        self.assertEqual(type(place1.longitude), float)
+        sef.assertEqual(type(place1. amnity_ids), list)
+
+    def test_value(self):
+        """this method will test whether values of attributes
+        are correct"""
+        place1 = Place()
+        self.assertEqual(place1.city_id, "")
+        self.assertEqual(place1.user_id, "")
+        self.assertEqual(place1.name, "")
+        self.assertEqual(place1.description, "")
+        self.assertEqual(place1.number_rooms, 0)
+        self.assertEqual(place1.number_bathrooms, 0)
+        self.assertEqual(place1.max_guest, 0)
+        self.asserEqual(place1.price_by_night, 0)
+        self.assertEqual(place1.latitude, 0.0)
+        self.assertEqual(place1.longitude, 0.0)
+        self.assertEqual(place1. amnity_ids, [])
